173a6dcf18f20d86a167e0e888691b3e2ce0a4f6
Daniel Cordeiro <daniel.cordeiro.dev@gmail.com>
2021-11-08 00:19:04 +0000

    Header component

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import "header.css";
+
+const Header = () => {
+  const title = "Daniel Cordeiro - Software engineer";
+
+  return (
+    <header >
+      <h1>{title}</h1>
+    </header>
+  )
+}
+
+export default Header;
