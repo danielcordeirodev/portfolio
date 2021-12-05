5b12923121c9d8ea1537d7f83ab11e039b1d041e
Daniel Cordeiro <daniel.cordeiro.dev@gmail.com>
2021-12-05 15:48:04 +0000

    Commented projectCard so it doenst appear

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ function App() {
     <div>
       <Header />
       <AboutMe />
-      <ProjectCard
-        project={{
-          title: "task-organizer",
-          image: "https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/can-task-be-a-verb-5813-7075c71d8cf734c3c83e9edf76bc66fb@1x.jpg",
-          description: "The best task organizer ever",
-        }}
-        visitProject={visitProject}
-      />
+      {/* <ProjectCard */}
+      {/*   project={{ */}
+      {/*     title: "task-organizer", */}
+      {/*     image: "https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/can-task-be-a-verb-5813-7075c71d8cf734c3c83e9edf76bc66fb@1x.jpg", */}
+      {/*     description: "The best task organizer ever", */}
+      {/*   }} */}
+      {/*   visitProject={visitProject} */}
+      {/* /> */}
     </div>
   );
 }
